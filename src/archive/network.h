16c1302c92c55d71fa44cad6d8a03b6b70b508f8
tom-oleson <tom.oleson@gmail.com>
2019-09-20 21:20:52 -0400

    Archive before deprecations

--- src/archive/network.h
+++ src/archive/network.h
@@ -37,6 +37,9 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <fcntl.h>
 #include <string.h>
 
 #include <vector>
@@ -54,16 +57,34 @@
 
 namespace cm_net {
 
-int enable_reuseaddr(int fd);
+
 int create_socket();
 int server_socket(int host_port);
 void close_socket(int fd);
 int accept(int host_socket, std::string &info);
 int connect(const std::string &host, int host_port, std::string &info);
+
 void send(int socket, const std::string &msg);
 void send(int socket, char *buf, size_t buf_size, const std::string &msg);
+
 int recv(int socket, char *buf, size_t buf_size);
-int recv_non_blocking(int socket, char *buf, size_t buf_size);
+int recv_non_block(int socket, char *buf, size_t buf_size);
+int read(int fd, char *buf, size_t sz);
+int write(int fd, char *buf, size_t sz);
+
+
+int enable_reuseaddr(int fd);
+int set_non_block(int fd, bool non_block);
+int set_keep_alive(int fd);
+int set_keep_alive_interval(int fd, int interval);
+int set_no_delay(int fd, int no_delay);
+int set_send_buffer(int fd, int size);
+int set_receive_buffer(int fd, int size);
+int set_send_timeout(int fd, long long millis);
+int set_receive_timeout(int fd, long long millis);
+int read(int fd, char *buf, size_t sz);
+int write(int fd, char *buf, size_t sz);
+
 
 inline void err(const std::string &msg, int errnum) {
     char buf[128] = {'\0'};
@@ -169,356 +190,6 @@ public:
 
 };
 
-// client/server model
-//======================================================================== 
-
-class duplex_connection {
-
-    struct tx_thread: public cm_thread::basic_thread {
-
-        char sbuf[4096] = { '\0' };
-        int socket;
-        cm_queue::double_queue<std::string> tx_queue;
-
-        size_t count = 0;
-        std::string out;
-        
-        // tx_send
-        bool process() {
-            while(!tx_queue.empty()) {
-                out = std::move(tx_queue.pop_front());
-                cm_net::send(socket, sbuf, sizeof(sbuf), out);
-                count += out.size();
-                if(count % 4096 == 0) {
-                    // tweak this to optimal
-                    timespec delay = {0, 2000000}; // 2 ms
-                    nanosleep(&delay, NULL);
-                }
-            }
-            return true;
-        }
-
-        tx_thread(int s): socket(s) { start(); };
-        ~tx_thread() { stop(); }
-    };
-
-    struct rx_thread: public cm_thread::basic_thread  {
-
-        char rbuf[4096] = { '\0' };
-        int socket;
-        cm_queue::double_queue<std::string> rx_queue;
-
-        size_t count = 0;
-        std::string in;
-
-        // rx_receive
-        bool process() {
-            int num_bytes = 0;
-            // blocking, waits for socket data
-            if(CM_NET_ERR != (num_bytes = cm_net::recv(socket, rbuf, sizeof(rbuf)))) {
-                in = std::move(std::string(rbuf, num_bytes));
-                rx_queue.push_back(in);
-                count += num_bytes;
-            }
-
-            if(num_bytes == 0) {
-                // disconnected, end this thread
-                return false;
-            }
-
-            return num_bytes != CM_NET_ERR;
-        }
-
-        rx_thread(int s): socket(s) { start(); };
-        ~rx_thread() { stop(); }
-    };
-
-    tx_thread *tx = nullptr;
-    rx_thread *rx = nullptr;
-    int socket;
-
-
-    bool setup() {
-        tx = new tx_thread(socket);
-        rx = new rx_thread(socket);
-
-        if(nullptr == rx) {
-            if(nullptr != tx) delete tx;
-            return false;
-        }  
-
-        return true;
-    }
-
-    void cleanup() {
-        if(nullptr != tx) delete tx;
-        if(nullptr != rx) delete rx;
-    }
-
-public:
-
-    // improve this test
-    bool is_done() { rx->is_done() && tx->is_done(); }
-
-    bool has_rx_data() { return !rx->rx_queue.empty(); }
-    bool has_tx_data() { return !tx->tx_queue.empty(); }
-
-    size_t rx_count() { return rx->count; }
-    size_t tx_count() { return tx->count; }
-
-
-    duplex_connection(int s): socket(s) { setup(); }
-
-    ~duplex_connection() { cleanup(); }
-
-    void send(const std::string &s) {
-        tx->tx_queue.push_back(s);
-    }
-
-    bool receive(std::string &s) {
-        if(has_rx_data()) {
-            s = rx->rx_queue.pop_front();
-            return true;
-        }
-        return false;
-    }
-
-};
-
-class client: public cm_thread::basic_thread {
-
-protected:
-
-    std::string host;
-    int host_port;
-    std::string info;
-    
-    int socket;
-    duplex_connection *duplex = nullptr;
-
-    bool connect() {
-        // connect to host
-        socket = cm_net::connect(host, host_port, info);
-        if(-1 == socket) {
-            return false;
-        } 
-  
-        duplex = new duplex_connection(socket);
-        if(nullptr == duplex) {
-            cm_log::error("client: create duplex_connection failed");
-            cm_net::close_socket(socket);
-            return false;
-        }
-        return true;
-    }
-    
-    bool setup() {
-
-        if(!connect())  return false;
-
-        cm_log::info(cm_util::format("client: connected to: %s (%d)",
-            info.size() > 0 ? info.c_str(): "host?:serv?", socket));
-
-        //wait for connection to stablize before we start sending data
-        timespec delay = {0, 100000000};   // 0.1 seconds
-        nanosleep(&delay, NULL);
-
-        return true;
-    }
-
-    void cleanup() { 
-        cm_net::close_socket(socket);
-        if(nullptr != duplex) {
-            delete duplex;
-            duplex = nullptr;
-        }
-    }
-
-    std::string in;
-
-    bool process() {
-        if(duplex->receive(in)) {
-            on_receive(in);
-        }
-
-        // to-do: add thread monitor and shutdown check
-
-        return true;
-    }
-    
-public:
-    client(const std::string _host, int _host_port):
-        host(_host), host_port(_host_port) { start(); }
-    ~client() {  stop();  }
-
-    size_t rx_count() { return duplex->rx_count(); }
-    size_t tx_count() { return duplex->tx_count(); }
-
-    void send(const std::string &msg) { duplex->send(msg); }
-
-    void send_request(const std::string &msg) {
-
-        size_t pre_count = rx_count();
-
-        send(msg);
-
-        for(int count = 1000; rx_count() == pre_count; --count) {
-            // delay for response
-            timespec delay = {0, 6000000};   // 6 ms
-            nanosleep(&delay, NULL);
-        }
-    }
-
-    virtual void on_receive(const std::string &msg) = 0; 
-};
-
-
-
-class server_client: public cm_thread::basic_thread {
-
-protected:
-  
-
-    duplex_connection *duplex = nullptr;
-
-    int socket;
-    std::string info;
-   
-
-    bool connect() {
-
-        duplex = new duplex_connection(socket);
-        if(nullptr == duplex) {
-            cm_log::error("server_client: create duplex_connection failed");
-            cm_net::close_socket(socket);
-            return false;
-        }
-        return true;
-    }
-    
-    bool setup() {
-
-        if(!connect())  return false;
-
-        cm_log::info(cm_util::format("server_client: connected to: %s (%d)",
-            info.size() > 0 ? info.c_str(): "host?:serv?", socket));
-
-        //wait for connection to stablize before we start sending data
-        timespec delay = {0, 100000000};   // 0.1 seconds
-        nanosleep(&delay, NULL);
-
-        return true;
-    }
-
-    void cleanup() { 
-        cm_net::close_socket(socket);
-        if(nullptr != duplex) {
-            delete duplex;
-            duplex = nullptr;
-        }
-    }
-
-    std::string in;
-
-    bool process() {
-        if(duplex->receive(in)) {
-            on_receive(in);
-        }
-
-        // to-do: add thread monitor and shutdown check
-
-        return true;
-    }
-
-void _send(const std::string &msg) { duplex->send(msg); }
-    
-public:
-    server_client(int _socket, const std::string _info) {
-        socket = _socket;
-        info = _info;
-        start();
-    }
-    ~server_client() {  stop();  }
-
-    size_t rx_count() { return duplex->rx_count(); }
-    size_t tx_count() { return duplex->tx_count(); }
-
-    void send(const std::string &msg) {
-
-        size_t pre_count = rx_count();
-
-        _send(msg);
-
-        for(int count = 1000; rx_count() == pre_count; --count) {
-            // delay for response
-            timespec delay = {0, 6000000};   // 6 ms
-            nanosleep(&delay, NULL);
-        }
-    }
-
-    virtual void on_receive(const std::string &msg) = 0; 
-};
-
-
-template<typename server_clientT>
-class server: public cm_thread::basic_thread  {
-
-protected:
-   
-    std::vector<std::unique_ptr<server_clientT>> connections;
-
-    int host_port;
-    int host_socket;
-    std::string info;
-  
-    bool setup() {
-
-        host_socket = cm_net::server_socket(host_port);
-        if(-1 == host_socket) {
-            return false;
-        }
-        return true;
-    }
-
-    void cleanup() { 
-        cm_net::close_socket(host_socket);
-    }
-
-    bool process() {
-
-        int socket = cm_net::accept(host_socket, info);
-        if(-1 == socket) {
-            cm_log::critical("accept failed in server thread, ending thread process");
-            return false;
-        }
-
-        // start a thread to handle this new connection
-        service_connection(socket, info);
-
-        return true;
-    }
-
-    void service_connection(int socket, const std::string info) {
-
-        cm_log::info(cm_util::format("server: connected to: %s (%d)",
-            info.size() > 0 ? info.c_str(): "host?:serv?", socket));
-
-        auto p = std::make_unique<server_clientT>(socket, info);
-        connections.push_back(std::move(p));
-       
-
-        //cleanup connections list
-        for(auto i = connections.begin(); i != connections.end(); i++) {
-            if((*i)->is_done()) {
-                connections.erase(i);
-            }
-        }
-    }
-    
-public:
-    server(int port): host_port(port) { start(); }
-    ~server() { stop(); }
-};
 
 
 } // namespace cm_net
